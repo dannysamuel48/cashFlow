6d54f0047fcd5fd1a8b1bd034c30e2b0e326fa2d
Danny Samuel <dannysamuel48@gmail.com>
2022-08-05 11:33:47 -0300

    Finalizado a Please enter the commit message for your changes. Lines starting

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <Suspense>
+    <template #default>
+      <Home></Home>
+    </template>
+    <template #fallback>
+      <SplashScreen></SplashScreen>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import SplashScreen from "./components/SplashScreen.vue";
+
+export default {
+  components: {
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 1500);
+        })
+    ),
+    SplashScreen,
+  },
+};
+</script>
+
+<style>
+html,
+body .app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
